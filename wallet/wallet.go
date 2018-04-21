ba77d8b732cabcd90e0e8e280c6f989f918120cc
riacataquian <icataquian@gmail.com>
2018-04-21 21:37:51 +0800

    Implements pkg wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,16 +1,28 @@
 package wallet
 
+import (
+	"fmt"
+)
+
+// Bitcoin ..
+type Bitcoin int
+
+// Stringer ..
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // Wallet ..
 type Wallet struct {
-	bal int
+	bal Bitcoin
 }
 
 // Deposit ..
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.bal += amount
 }
 
 // Balance ..
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.bal
 }
