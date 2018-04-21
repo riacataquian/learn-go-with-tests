5f69183e674bfd70c6a0485525dd2c1e565230d1
riacataquian <icataquian@gmail.com>
2018-04-21 20:42:33 +0800

    Refactor tests -> table-driven structs

--- shapes/perimeter_test.go
+++ shapes/perimeter_test.go
@@ -17,19 +17,34 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	tests := []struct {
+		desc string
 		s    Shape
 		want float64
 	}{
-		{Rectangle{12, 6}, 72.00},
-		{Circle{10}, 314.16},
-		{Triangle{12, 6}, 36.0},
+		{
+			desc: "Calculate for the area of a Rectangle",
+			s:    Rectangle{12, 6},
+			want: 72.00,
+		},
+		{
+			desc: "Calculate for the area of a Circle",
+			s:    Circle{10},
+			want: 314.16,
+		},
+		{
+			desc: "Calculate for the area of a Triangle",
+			s:    Triangle{12, 6},
+			want: 36.0,
+		},
 	}
 
 	for _, test := range tests {
-		got := test.s.Area()
+		t.Run(test.desc, func(t *testing.T) {
+			got := test.s.Area()
 
-		if math.Round(got) != math.Round(test.want) {
-			t.Errorf("got %.2f, want %.2f", got, test.want)
-		}
+			if math.Round(got) != math.Round(test.want) {
+				t.Errorf("%s: Area() = %.2f, want %.2f", test.desc, got, test.want)
+			}
+		})
 	}
 }
