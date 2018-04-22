cc939fe5999b6771cc0c4ae8d2996803613f8edf
riacataquian <icataquian@gmail.com>
2018-04-22 15:28:41 +0800

    Adds func and type docs

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,33 +5,34 @@ import (
 	"fmt"
 )
 
-// ErrInsufficient ..
+// ErrInsufficient is the error to be returned for insufficient funds.
 var ErrInsufficient = errors.New("cannot withdraw, insufficient funds")
 
-// Bitcoin ..
+// Bitcoin represents the balance of a Wallet.
 type Bitcoin int
 
-// Stringer ..
+// String formats Bitcoin when printed as string.
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
-// Wallet ..
+// Wallet encapsulates a balance.
 type Wallet struct {
 	bal Bitcoin
 }
 
-// Deposit ..
+// Deposit adds amount to the Wallet's current balance.
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.bal += amount
 }
 
-// Balance ..
+// Balance returns the current balance of a Wallet.
 func (w *Wallet) Balance() Bitcoin {
 	return w.bal
 }
 
-// Withdraw ..
+// Withdraw substracts the amount from the Wallet's current balance.
+// Returns an error if funds is insufficient.
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.bal {
 		return ErrInsufficient
