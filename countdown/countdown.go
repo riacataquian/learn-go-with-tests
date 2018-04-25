392154f5e27d6966d8740016cdc2a8f39b05bc0b
riacataquian <icataquian@gmail.com>
2018-04-25 18:14:07 +0800

    Cleanups sleeper implementation

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -4,13 +4,50 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func main() {
-	Countdown(os.Stdout)
+const (
+	start     = 3
+	finalWord = "Go!"
+)
+
+// Sleeper ...
+type Sleeper interface {
+	Sleep()
+}
+
+// SpySleeper ...
+type SpySleeper struct {
+	Calls int
+}
+
+// Sleep ...
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+// ConfigurableSleeper ...
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+// Sleep ...
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
 }
 
 // Countdown ....
-func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := start; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprintf(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout, &ConfigurableSleeper{1 * time.Second})
 }
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -7,13 +7,21 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	sleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, sleeper)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+Go!`
 
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
+	}
+
+	if sleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, got %d want 4", sleeper.Calls)
 	}
 }
