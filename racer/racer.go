7a76859e69d7e89206a66ef37656506afe7d6368
riacataquian <icataquian@gmail.com>
2018-04-29 14:39:57 +0800

    Use select construct and channels over time.Duration

--- racer/racer.go
+++ racer/racer.go
@@ -2,22 +2,27 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (faster string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select { // lets you wait on multiple channels.
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+// ping starts a goroutine that will send ch a signal that http.Get request is finished.
+func ping(url string) chan bool {
+	ch := make(chan bool)
+
+	go func() {
+		http.Get(url)
+
+		// blocks until http.Get is done.
+		ch <- true
+	}()
+
+	return ch
 }
