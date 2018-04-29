da5851f0c260596a1487a41092cfe68270d16939
riacataquian <icataquian@gmail.com>
2018-04-29 15:06:27 +0800

    Adds ConfigurableRacer

--- racer/racer.go
+++ racer/racer.go
@@ -1,15 +1,27 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(a, b string) (faster string) {
+var defaultTimeout = 10 * time.Second
+
+// Racer determine the faster website between a and b url with a defaultTimeout.
+func Racer(a, b string) (faster string, err error) {
+	return ConfigurableRacer(a, b, defaultTimeout)
+}
+
+// ConfigurableRacer determine the faster website between a and b url given a defaultTimeout.
+func ConfigurableRacer(a, b string, timeout time.Duration) (faster string, err error) {
 	select { // lets you wait on multiple channels.
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout): // https://golang.org/pkg/time/#After
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
--- racer/racer_test.go
+++ racer/racer_test.go
@@ -8,18 +8,38 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("returns the fastest server", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	got := Racer(slowServer.URL, fastServer.URL)
-	want := fastServer.URL
+		got, err := Racer(slowServer.URL, fastServer.URL)
+		want := fastServer.URL
 
-	if got != want {
-		t.Errorf("got '%s', want '%s'", got, want)
-	}
+		if err != nil {
+			t.Errorf("got %v, want nil", err)
+		}
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("got nil, want error")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
