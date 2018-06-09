f6fa9576053f6e648fa8d339a439fad6c484c305
riacataquian <icataquian@gmail.com>
2018-06-09 13:54:23 +0800

    Adds main package

---
+++ httpserver/main.go
@@ -0,0 +1,14 @@
+// Package main ...
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen to port 5000: %v", err)
+	}
+}
--- httpserver/server.go
+++ httpserver/server.go
@@ -1,11 +1,11 @@
-// Package server ...
-package server
+package main
 
 import (
 	"fmt"
 	"net/http"
 )
 
+// PlayerServer ...
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "20")
 }
